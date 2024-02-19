a609e3df2986fc06df8b2731a65d136543d7e542
“kazsho” <kshomade@gmail.com>
2024-02-19 14:54:48 +0000

    H1 and h2 tags added

--- src/Display.jsx
+++ src/Display.jsx
@@ -3,4 +3,13 @@ import { useState } from "react"
 
 export default function Display(){
     
+
+
+return (
+    <div className="Display">
+        <h1>Kanye West</h1>
+        <h2>Popular</h2>
+        
+    </div>
+)
 }
